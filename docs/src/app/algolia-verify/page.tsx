6a9bfcc0e7aa8c4f7e5864540318269493c51297
Karol Broda <karol.broda@titanom.com>
2025-07-18 18:21:36 +0200

    docs: add algolia verification page with metadata

---
+++ docs/src/app/algolia-verify/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  other: {
+    "algolia-site-verification": "E9E4935BFD0C6685",
+  },
+};
+
+export default function AlogiaVerifyPage() {
+  return <div>AlogiaVerifyPage</div>;
+}
