2059279639e678474e3b797f6de5a80cf71a625c
Karol Broda <karol.broda@titanom.com>
2026-05-12 22:09:07 +0200

    feat(ci): move clippy and fmt checks from nix derivations to direct cargo commands

--- nix/parts/checks.nix
+++ nix/parts/checks.nix
@@ -3,24 +3,13 @@
     pkgs,
     lib,
     system,
-    craneLib,
-    built,
     ...
   }: {
-    checks =
-      {
-        clippy = craneLib.cargoClippy (built.commonArgs
-          // {
-            inherit (built) cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets";
-          });
-        fmt = craneLib.cargoFmt {inherit (built.commonArgs) src pname version;};
-      }
-      // lib.optionalAttrs pkgs.stdenv.isLinux {
-        nixos-server = import ../tests/server.nix {
-          inherit pkgs system;
-          self = inputs.self;
-        };
+    checks = lib.optionalAttrs pkgs.stdenv.isLinux {
+      nixos-server = import ../tests/server.nix {
+        inherit pkgs system;
+        self = inputs.self;
       };
+    };
   };
 }
